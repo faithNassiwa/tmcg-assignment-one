8496a15ee947c14a4bc2f9dec546af82b354151d
Faith Nassiwa <faithnassiwa@gmail.com>
2017-01-06 16:20:06 +0300

    Update assignment_one.py

--- assignment_one.py
+++ assignment_one.py
@@ -1,10 +1,10 @@
 '''
 A python function that takes a list of SMS messages and input and returns 
-a ranking of each words frequency
+a ranking of each word's frequency
 '''
 
 def wordFreqDict(message_list): 
-	message_string = ', '.join(message_list)  # converting list of messages to a string
+	message_string = ', '.join(message_list)  # converting list(message_list) to a string
 	word_list = message_string.split()  # splits message_string into a list of words
 	word_freq = [word_list.count(word) for word in word_list]  # count the frequency of each word's appearance in the word_list
 
