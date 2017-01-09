95e367c7fd5f251fbb7ca61f610451553f4ae32b
Faith Nassiwa <faithnassiwa@gmail.com>
2017-01-09 13:55:05 +0300

    Update assignment_one.py

--- assignment_one.py
+++ assignment_one.py
@@ -4,7 +4,7 @@ a ranking of each word's frequency in a dictionary
 '''
 import re
 
-def wordFreqDict(message_list): 
+def WordFreqDict(message_list): 
 	message_string = ' , '.join(message_list)  # converting the list (message_list) to a string
 	word_list = re.split("[, \-!?:]+", message_string)  # splitting message_string into a list of words minus provided punctuations
 	word_freq = [word_list.count(word) for word in word_list]  # counting the frequency of each word's appearance in the word_list
